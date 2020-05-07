40c60b3ae55f0a43753ef2f8c88f748e15a97399
Riya <kato.hana@outlook.jp>
2020-05-07 23:11:04 +0200

    Added log channel attachment logic

--- events/Ready.js
+++ events/Ready.js
@@ -1,4 +1,5 @@
 const { Event, Logger } = require('chariot.js');
+const { guildModeID, logChannelID } = require('../config/config');
 
 class Ready extends Event {
     constructor() {
@@ -6,7 +7,23 @@ class Ready extends Event {
     }
 
     async execute() {
-        Logger.event(`EASYHARD.NET Bot is ready in ${this.client.guilds.size} ${this.client.guilds.size === 1 ? 'guild' : 'guilds'}!`); 
+        Logger.event(`EASYHARD.NET Bot is ready in ${this.client.guilds.size} ${this.client.guilds.size === 1 ? 'guild' : 'guilds'}!`);
+
+        this.client.editStatus('online', { name: 'Minecraft', type: 0 });
+
+        const guildToLogIn = this.client.guilds.get(guildModeID);
+
+        if (!guildToLogIn) {
+            return Logger.warn('INVALID GUILD ID', `Couldn't retrieve guild by the ID specified in the configuration. Please validate for any typos!`);
+        }
+
+        const logChannel = guildToLogIn.channels.get(logChannelID);
+
+        if (!logChannel) {
+            return Logger.warn('INVALID LOG CHANNEL ID', `Couldn't retrieve log channel by the ID specified in the configuration. Please validate for any typos!`);
+        }
+
+        this.client.logChannel = logChannel;
     }
 }
 
