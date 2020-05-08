572b6f9a9585951f4c0ccaea0ffe7cf647bfd216
Riya <kato.hana@outlook.jp>
2020-05-08 12:35:07 +0200

    Added banner image URL

--- constants/Links.js
+++ constants/Links.js
@@ -1,5 +1,6 @@
 module.exports = {
     FACEBOOK: 'https://pl-pl.facebook.com/EasyHardMC',
     WEBSITE: 'https://easyhard.net',
-    INVITE: 'https://discord.gg/CdeQCwv'
+    INVITE: 'https://discord.gg/CdeQCwv',
+    BANNER_IMAGE: 'https://i.imgur.com/ZMEDRST.png'
 }
