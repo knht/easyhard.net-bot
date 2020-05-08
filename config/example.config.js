a1a5ecc5de04020d0c5d024b7b5fc67d500ea7a9
Riya <kato.hana@outlook.jp>
2020-05-08 17:02:30 +0200

    Updated default config

--- config/example.config.js
+++ config/example.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-    token: '',
-    logChannelID: '',
-    roleOnJoinID: '',
-    guildModeID: '',
-    chariotOptions: {
-        prefix: '',
-        defaultHelpCommand: false,
-        owner: [
+    token: '',                              // Discord API bot application token
+    logChannelID: '',                       // Channel ID where the logs should be posted in
+    roleOnJoinID: '',                       // Role ID of the role that should be assigned upon joining
+    guildModeID: '',                        // Guild ID of the guild the bot should operate in
+    chariotOptions: {       
+        prefix: 'e!',                       // Command prefix
+        defaultHelpCommand: false,          // Disabling the default help command
+        owner: [                            // A list of user IDs "owning" the bot (e.g. for owner level commands)
             '',
         ],
-        excludeDirectories: [
+        excludeDirectories: [               // Don't touch this
             'config'
         ]
     },
-    erisOptions: {}
+    erisOptions: {                          // Don't touch this
+        messageLimit: 2500,
+        restMode: true
+    }
 }
